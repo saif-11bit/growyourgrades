fae4b73b026f980cf26b2f15f8766b3de9d83b42
saif-11bit <56977968+saif-11bit@users.noreply.github.com>
2020-10-16 07:52:56 +0530

    Update settings.py

--- online_edu/settings.py
+++ online_edu/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bfxp81$97*k@6@#n0&q@i(btcmnke*jr!j_=76+@gf!82hjva6'
+SECRET_KEY = 'SecretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
